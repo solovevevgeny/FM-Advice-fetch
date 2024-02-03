5a820d45d71bf717b3bc5196324dda9f5b1496e0
Evgeny Solovev <evgeny.solovev@gmail.com>
2024-02-03 13:26:06 +0300

    ready

---
+++ adiveJSON.jsx
@@ -0,0 +1,18 @@
+window.addEventListener('load', function () {
+
+    async function fetchAdvice(url) {
+        await fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+          let cardTitleDiv = document.querySelector('.card__title');
+          let cardTextDiv = document.querySelector('.card__text');
+
+          cardTitleDiv.innerHTML += data.slip.id;
+          cardTextDiv.innerHTML = data.slip.advice;
+
+        });
+      }
+
+      fetchAdvice('https://api.adviceslip.com/advice');
+});
